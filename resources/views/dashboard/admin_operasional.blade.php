f02e5cc91df079bada4446ec3b2f6f20d0495fbb
NiKomangCahyasari12 <gcahya908@gmail.com>
2025-12-29 01:08:52 +0700

    perubahan admin

--- resources/views/dashboard/admin_operasional.blade.php
+++ resources/views/dashboard/admin_operasional.blade.php
@@ -1,20 +1,30 @@
 <x-app-layout>
-    {{-- <div class="bg-blue-50 p-6">
-        <div class="max-w-7xl mx-auto bg-white rounded-lg shadow-lg p-6">
-            <h1 class="text-2xl font-bold text-blue-700 mb-4">Admin Operasional Dashboard</h1>
-            <a href="{{ route('hotels.index') }}" class="bg-blue-600 text-white px-4 py-2 rounded">
-                Kelola Hotel & Kamar
-            </a>
-        </div>
-    </div> --}}
-
     <div class="max-w-7xl mx-auto px-6 mt-6 grid grid-cols-1 md:grid-cols-3 gap-6">
         @foreach($hotels as $hotel)
-        <div class="bg-white rounded-lg shadow p-4">
-            <h2 class="font-bold text-lg">{{ $hotel->nama_hotel }}</h2>
-            <p>Total Kamar: {{ $hotel->rooms->count() }}</p>
-            <p>Fasilitas: {{ $hotel->fasilitas }}</p>
-        </div>
+            <div class="bg-white rounded-lg shadow overflow-hidden">
+
+                {{-- GAMBAR HOTEL --}}
+                @if($hotel->gambar)
+                    <img src="{{ asset('storage/'.$hotel->gambar) }}"
+                         class="w-full h-40 object-cover">
+                @else
+                    <div class="w-full h-40 bg-gray-200 flex items-center justify-center text-gray-500">
+                        Tidak ada gambar
+                    </div>
+                @endif
+
+                {{-- INFO HOTEL --}}
+                <div class="p-4">
+                    <h2 class="font-bold text-lg">
+                        {{ $hotel->nama_hotel }}
+                    </h2>
+
+
+                    <p class="text-sm mt-1">
+                        Fasilitas: {{ $hotel->fasilitas }}
+                    </p>
+                </div>
+            </div>
         @endforeach
     </div>
 </x-app-layout>
